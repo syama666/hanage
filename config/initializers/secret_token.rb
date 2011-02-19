44f3f6ff444de1779bdbd9c3b9c721058f6ddd9d
syama666 <yoshinorisugiyama@gmail.com>
2011-02-20 03:20:12 +0900

    commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanage::Application.config.secret_token = '4fb213ab411704f23ad77a9f271e32c3a3f80527bad42853c317c0667e7c169f5ac39793fe8e93445c05542fcce9ec624419e9ef9900378726f1bc43f2f62dac'
